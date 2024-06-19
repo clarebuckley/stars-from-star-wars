2a99356d7acaa7b712472d250808cfd9e218045d
Clare <clarebuckley1@hotmail.com>
2024-06-19 21:31:37 +0100

    Added ability to like characters using pinia

--- stars-of-star-wars/src/components/ListComponent.vue
+++ stars-of-star-wars/src/components/ListComponent.vue
@@ -5,9 +5,42 @@
         <v-row>
             <v-col cols="6">
                 <v-card class="pa-2">
+                    <h2>Liked Characters</h2>
+                    <p v-if="likedCharactersStore.getLikedCharacters.length == 0">You have not liked any characters. Use the heart icon to like a character.</p>
+                    <v-list lines="three">
+                        <v-list-item v-for="character in likedCharactersStore.getLikedCharacters" :key="character.url" :title="character.name"
+                            @click="updateSelectedCharacter(character.url)">
+                            <v-list-item-title>
+                                <div>
+                                    {{ character.name }}
+                                </div>
+                                <div> 
+                                    <v-btn v-if="!character.liked" @click="addLikedCharacter(character)" density="compact" icon="mdi-heart-outline"></v-btn>
+                                    <v-btn v-else @click="removeLikedCharacter(character)" density="compact" icon="mdi-heart"></v-btn>
+                                </div>
+                                
+                            </v-list-item-title>
+                            <v-list-item-subtitle>
+                                <p>Stars in {{ character.films.length }} film<span v-if="character.films.length > 1">s</span></p>
+                            </v-list-item-subtitle>
+                        </v-list-item>
+                    </v-list>
+                </v-card>
+                <v-card class="pa-2">
+                    <h2>All Characters</h2>
                     <v-list lines="three">
                         <v-list-item v-for="character in characters" :key="character.url" :title="character.name"
                             @click="updateSelectedCharacter(character.url)">
+                            <v-list-item-title>
+                                <div>
+                                    {{ character.name }}
+                                </div>
+                                <div> 
+                                    <v-btn v-if="!character.liked" @click="addLikedCharacter(character)" density="compact" icon="mdi-heart-outline"></v-btn>
+                                    <v-btn v-else @click="removeLikedCharacter(character)" density="compact" icon="mdi-heart"></v-btn>
+                                </div>
+                                
+                            </v-list-item-title>
                             <v-list-item-subtitle>
                                 <p>Stars in {{ character.films.length }} film<span v-if="character.films.length > 1">s</span></p>
                             </v-list-item-subtitle>
@@ -36,6 +69,7 @@ import { getAllCharacters } from '../services/characters/Characters.api.ts';
 import { ref, watch } from 'vue';
 import ErrorMessage from './ErrorMessage.vue';
 import CharacterDetailsComponent from './CharacterDetailsComponent.vue';
+import { useLikedCharactersStore } from '@/stores/likedCharacters'
 
 const characters = ref([]);
 const currentPage = ref(1);
@@ -45,6 +79,7 @@ const selectedCharacter = ref(null);
 const errored = ref(false);
 const pageLoading = ref(true);
 const errorMessage = ref("");
+const likedCharactersStore = useLikedCharactersStore();
 
 async function getData() {
     pageLoading.value = true;
@@ -59,11 +94,20 @@ async function getData() {
         totalPages.value = Math.ceil(charactersResponse.count / 10);
         pageLoading.value = false;
     }
-
 }
 
 function updateSelectedCharacter(newSelectedCharacter) {
     selectedCharacter.value = characters.value.find(x => x.url == newSelectedCharacter);
+}
+
+function addLikedCharacter(characterToLike){
+    characterToLike.liked = true;
+    likedCharactersStore.addLikedCharacter(characterToLike);
+}
+
+function removeLikedCharacter(characterToRemove){
+    characterToRemove.liked = false;
+    likedCharactersStore.removeLikedCharacter(characterToRemove.url);
 }
 
 function handleError(error) {
---
+++ stars-of-star-wars/src/stores/likedCharacters.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+import { Character } from '@/types/Character';
+
+export const useLikedCharactersStore = defineStore('likedCharacters', {
+  state: () => ({
+    likedCharacters: [] as Character[]
+  }),
+  getters: {
+    getLikedCharacters(state): Character[] {
+      return this.likedCharacters;
+    }
+  },
+  actions: {
+    addLikedCharacter(character: Character) {
+      this.likedCharacters.push(character);
+    },
+    removeLikedCharacter(characterUrl: string) {
+      this.likedCharacters = this.likedCharacters.filter((character) => character.url !== characterUrl);
+    },
+    isLikedCharacter(characterUrl: string): boolean {
+      return this.likedCharacters.some(x => x.url === characterUrl)
+    }
+  },
+})
